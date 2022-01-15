c1f048ae7b1b699791f4ce40b9c4207fb0790d37
Elijah Walker <advancedathletix@gmail.com>
2022-01-15 17:04:59 -0500

    added letters and symbols to array

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function generatePassword() {
   var symbols = confirm("Would you like to use symbols in your password?")
 
   // arrays of characters to pick from
-var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H",]
-var lowercaseArray = ["a", "b", "c", "d"]
-var symbolArray = ["!", ]
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var symbolArray = ["!", "", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", ":", ":", "<", ">", "=", "?", "@", ]
 var numberArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var chosenArray = []
 
